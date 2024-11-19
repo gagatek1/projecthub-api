f4e344d70487757021bf6adad9f2bdcabf5b6f1a
gagatek1 <jakub.gagat@outlook.com>
2024-11-19 19:40:19 +0100

    Add show projects endpoint

--- app/api/v1/endpoints/project_endpoint.py
+++ app/api/v1/endpoints/project_endpoint.py
@@ -3,6 +3,7 @@ from starlette import status
 
 from app.models.project import Project
 from app.services.project.create_service import create_service
+from app.services.project.get_service import get_projects
 
 project_router = APIRouter(prefix="/projects", tags=["projects"])
 
@@ -12,3 +13,10 @@ async def create_project(project: Project):
     project = create_service(project)
 
     return project
+
+
+@project_router.get("/")
+async def show_projects():
+    projects = get_projects()
+
+    return projects
