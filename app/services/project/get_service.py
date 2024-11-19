c09bb1ceaeecdd89de503b00f3f70adc19f3972b
gagatek1 <jakub.gagat@outlook.com>
2024-11-19 19:39:15 +0100

    Add get service

--- app/services/project/get_service.py
+++ app/services/project/get_service.py
@@ -1,16 +1,14 @@
 from botocore.exceptions import ClientError
 from fastapi.responses import JSONResponse
-from boto3.dynamodb.conditions import Key
 
 from app.core.database import dynamodb
 
 table = dynamodb.Table("projects")
 
+
 def get_projects():
     try:
-        response = table.scan(
-            AttributesToGet=["id", "name"]
-        )
+        response = table.scan(AttributesToGet=["id", "name"])
         return response["Items"]
     except ClientError as e:
         return JSONResponse(content=e.response["Error"], status_code=500)
