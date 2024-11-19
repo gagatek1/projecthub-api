4d09ef021603d0ccd20132c3ca57270d50510c3c
gagatek1 <jakub.gagat@outlook.com>
2024-11-19 19:29:40 +0100

    Add include router

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
+from app.api.v1.router import router
 from app.core.database import create_tables
 
 app = FastAPI()
 
 handler = Mangum(app)
+
+app.include_router(router)
 
 create_tables()
