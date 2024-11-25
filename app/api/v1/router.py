6b513db52f5fd6aacf672ce8dd67a36089d18e14
gagatek1 <jakub.gagat@outlook.com>
2024-11-25 16:45:35 +0100

    Include auth router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from app.api.v1.endpoints.auth_endpoint import auth_router
 from app.api.v1.endpoints.project_endpoint import project_router
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(project_router)
+router.include_router(auth_router)
