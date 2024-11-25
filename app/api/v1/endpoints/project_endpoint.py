d590ff6a88631426b38b8b5e1a46ed36a0b6c599
gagatek1 <jakub.gagat@outlook.com>
2024-11-25 15:46:55 +0100

    Add update endpoint

--- app/api/v1/endpoints/project_endpoint.py
+++ app/api/v1/endpoints/project_endpoint.py
@@ -5,6 +5,7 @@ from app.models.project import Project
 from app.services.project.create_service import create_service
 from app.services.project.delete_service import delete_service
 from app.services.project.get_service import get_project, get_projects
+from app.services.project.update_service import update_service
 
 project_router = APIRouter(prefix="/projects", tags=["projects"])
 
@@ -33,3 +34,10 @@ async def show_project(project_id: str):
 @project_router.delete("/delete/{project_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_project(project_id: str):
     delete_service(project_id)
+
+
+@project_router.put("/update")
+async def update_project(project: Project):
+    project = update_service(project)
+
+    return project
