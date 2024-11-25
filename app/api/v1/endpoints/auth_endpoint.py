9f1df2d74356db5970ff2bff6adb024b265918d7
gagatek1 <jakub.gagat@outlook.com>
2024-11-25 18:37:02 +0100

    Add endpoint to verify user

--- app/api/v1/endpoints/auth_endpoint.py
+++ app/api/v1/endpoints/auth_endpoint.py
@@ -3,8 +3,9 @@ from starlette import status
 
 from app.core.cognito import Cognito
 from app.core.dependencies import get_cognito
-from app.models.user import UserSignup
+from app.models.user import UserSignup, UserVerify
 from app.services.auth.signup_service import signup_service
+from app.services.auth.verify_service import verify_service
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -12,3 +13,8 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 @auth_router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def signup_user(user: UserSignup, cognito: Cognito = Depends(get_cognito)):
     return signup_service(user, cognito)
+
+
+@auth_router.post("/verify")
+async def verify_user(data: UserVerify, cognito: Cognito = Depends(get_cognito)):
+    return verify_service(data, cognito)
