2dbe7faa77545a191a0c5b0ebea639b1fd11e70b
gagatek1 <jakub.gagat@outlook.com>
2024-11-26 00:56:54 +0100

    Add new token service

---
+++ app/services/auth/new_token_service.py
@@ -0,0 +1,32 @@
+import botocore
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+
+from app.core.cognito import Cognito
+from app.models.user import UserRefreshToken
+
+
+def new_token_service(data: UserRefreshToken, cognito: Cognito):
+    try:
+        response = cognito.new_token(data)
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "InvalidParameterException":
+            raise HTTPException(
+                status_code=400, detail="Refresh token provided has wrong format"
+            )
+        elif e.response["Error"]["Code"] == "NotAuthorizedException":
+            raise HTTPException(
+                status_code=401, detail="Invalid refresh token provided"
+            )
+        elif e.response["Error"]["Code"] == "LimitExceededException":
+            raise HTTPException(
+                status_code=429, detail="Attempt limit exceeded, please try again later"
+            )
+        else:
+            raise HTTPException(status_code=500, detail="Internal Server")
+    else:
+        content = {
+            "AccessToken": response["AuthenticationResult"]["AccessToken"],
+            "ExpiresIn": response["AuthenticationResult"]["ExpiresIn"],
+        }
+        return JSONResponse(content=content, status_code=200)
