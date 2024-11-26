077c07f270c8b1f5d5b513e94790c66868fba6bf
gagatek1 <jakub.gagat@outlook.com>
2024-11-26 01:39:49 +0100

    Add forgot password endpoint

--- app/api/v1/endpoints/auth_endpoint.py
+++ app/api/v1/endpoints/auth_endpoint.py
@@ -3,12 +3,20 @@ from starlette import status
 
 from app.core.cognito import Cognito
 from app.core.dependencies import get_cognito
-from app.models.user import UserRefreshToken, UserSignin, UserSignup, UserVerify, UserChangePassword
+from app.models.user import (
+    UserChangePassword,
+    UserForgotPassword,
+    UserRefreshToken,
+    UserSignin,
+    UserSignup,
+    UserVerify,
+)
+from app.services.auth.change_password_service import change_password_service
+from app.services.auth.forgot_password_service import forgot_password_service
 from app.services.auth.new_token_service import new_token_service
 from app.services.auth.signin_service import signin_service
 from app.services.auth.signup_service import signup_service
 from app.services.auth.verify_service import verify_service
-from app.services.auth.change_password_service import change_password_service
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -29,9 +37,21 @@ async def signin_user(data: UserSignin, cognito: Cognito = Depends(get_cognito))
 
 
 @auth_router.post("/token")
-async def generate_new_token(data: UserRefreshToken, cognito: Cognito = Depends(get_cognito)):
+async def generate_new_token(
+    data: UserRefreshToken, cognito: Cognito = Depends(get_cognito)
+):
     return new_token_service(data, cognito)
 
+
 @auth_router.post("/change")
-async def change_password(data: UserChangePassword, cognito: Cognito = Depends(get_cognito)):
+async def change_password(
+    data: UserChangePassword, cognito: Cognito = Depends(get_cognito)
+):
     return change_password_service(data, cognito)
+
+
+@auth_router.post("/forgot")
+async def forgot_password(
+    data: UserForgotPassword, cognito: Cognito = Depends(get_cognito)
+):
+    return forgot_password_service(data, cognito)
