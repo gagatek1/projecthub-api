df07ce0dd4c83b55a3cb4ca8943662cd2c270f15
Jakub Gagat <jakub.gagat@outlook.com>
2025-01-24 16:00:24 +0100

    Add get endpoint

--- app/api/v1/endpoints/task_endpoint.py
+++ app/api/v1/endpoints/task_endpoint.py
@@ -3,6 +3,7 @@ from starlette import status
 
 from app.models.task import Task
 from app.services.task.create_service import create_service
+from app.services.task.get_service import get_task, get_tasks
 
 task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
@@ -10,5 +11,19 @@ task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 @task_router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_task(task: Task):
     task = create_service(task)
+
+    return task
+
+
+@task_router.get("/")
+async def show_tasks():
+    tasks = get_tasks()
+
+    return tasks
+
+
+@task_router.get("/{task_id}")
+async def show_task(task_id: str):
+    task = get_task(task_id)
 
     return task
