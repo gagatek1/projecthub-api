11f89238395211e7490e33e6c714d0f83a0e4d0b
Jakub Gagat <jakub.gagat@outlook.com>
2025-01-24 15:47:46 +0100

    Edit task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
+from uuid import uuid4
+
+from pydantic import BaseModel, Field
+
+
+def generate_id():
+    return str(uuid4())
 
 
 class Task(BaseModel):
+    id: str = Field(default_factory=generate_id)
     name: str
     description: str
     project: str
