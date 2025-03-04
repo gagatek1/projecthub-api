a0ac1be9f444d40cad27a7c4cd1b8fcbb254030b
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-04 22:11:35 +0100

    Add optional variable

--- app/models/task.py
+++ app/models/task.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from uuid import uuid4
 
 from pydantic import BaseModel, Field
@@ -11,5 +12,5 @@ class Task(BaseModel):
     id: str = Field(default_factory=generate_id)
     name: str
     description: str
-    project_id: str
+    project_id: Optional[str] = None
     assigned_user: str
