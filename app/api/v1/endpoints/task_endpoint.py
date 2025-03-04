f613f9e440cbff69497c7b032ff1fedc7e4fb6c1
Jakub Gagat <jakub.gagat@outlook.com>
2025-03-04 22:13:04 +0100

    Add update endpoint

--- app/api/v1/endpoints/task_endpoint.py
+++ app/api/v1/endpoints/task_endpoint.py
@@ -5,6 +5,7 @@ from app.models.task import Task
 from app.services.task.create_service import create_service
 from app.services.task.delete_service import delete_service
 from app.services.task.get_service import get_task, get_tasks
+from app.services.task.update_service import update_service
 
 task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
@@ -33,3 +34,9 @@ async def show_task(task_id: str):
 @task_router.delete("/delete/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(task_id: str):
     delete_service(task_id)
+
+@task_router.put("/update")
+async def update_project(task: Task):
+    task = update_service(task)
+
+    return task
