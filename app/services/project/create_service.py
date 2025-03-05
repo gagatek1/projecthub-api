b35fcb3ae994c2fffbdfea2037c5ea343c30aef4
gagatek1 <jakub.gagat@outlook.com>
2025-03-05 19:10:13 +0100

    Add project owner variable

--- app/services/project/create_service.py
+++ app/services/project/create_service.py
@@ -4,14 +4,18 @@ from fastapi.responses import JSONResponse
 from app.core.database import dynamodb
 
 
-def create_service(project):
+def create_service(project, user):
     table = dynamodb.Table("projects")
 
     try:
         project_dict = project.dict()
 
+        user_id = user.get("Username")
+
+        project_dict["project_owner"] = user_id
+
         table.put_item(Item=project_dict)
 
-        return project
+        return project_dict
     except ClientError as e:
         return JSONResponse(content=e.response["Error"], status_code=500)
