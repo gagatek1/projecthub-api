caa76c1cc89ba94b8515242e67a549081039a697
gagatek1 <jakub.gagat@outlook.com>
2025-03-05 20:26:13 +0100

    Add authorization for endpoints

--- app/api/v1/endpoints/task_endpoint.py
+++ app/api/v1/endpoints/task_endpoint.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from starlette import status
 
+from app.core.security import get_token
 from app.models.task import Task
 from app.services.task.create_service import create_service
 from app.services.task.delete_service import delete_service
@@ -11,33 +12,33 @@ task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
 @task_router.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_task(task: Task):
-    task = create_service(task)
+async def create_task(task: Task, user: dict = Depends(get_token)):
+    task = create_service(task, user)
 
     return task
 
 
 @task_router.get("/")
-async def show_tasks():
+async def show_tasks(user: dict = Depends(get_token)):
     tasks = get_tasks()
 
     return tasks
 
 
 @task_router.get("/{task_id}")
-async def show_task(task_id: str):
+async def show_task(task_id: str, user: dict = Depends(get_token)):
     task = get_task(task_id)
 
     return task
 
 
 @task_router.delete("/delete/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_task(task_id: str):
-    delete_service(task_id)
+async def delete_task(task_id: str, user: dict = Depends(get_token)):
+    delete_service(task_id, user)
 
 
 @task_router.put("/update")
-async def update_project(task: Task):
-    task = update_service(task)
+async def update_project(task: Task, user: dict = Depends(get_token)):
+    task = update_service(task, user)
 
     return task
