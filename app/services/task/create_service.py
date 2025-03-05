c248609e86059bc655410cc9b0da30255e4b8814
gagatek1 <jakub.gagat@outlook.com>
2025-03-05 20:24:16 +0100

    Auto place assigned user when is None

--- app/services/task/create_service.py
+++ app/services/task/create_service.py
@@ -13,7 +13,6 @@ def create_service(task, user):
         if task_dict.get("assigned_user") is None:
             user_id = user.get("Username")
             task_dict["assigned_user"] = user_id
-            
 
         table.put_item(Item=task_dict)
 
