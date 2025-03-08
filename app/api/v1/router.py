dd9db9ae11f20374686d5dff0c8c12d68d22de81
gagatek1 <jakub.gagat@outlook.com>
2025-03-08 23:50:20 +0100

    Add include user router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.api.v1.endpoints.auth_endpoint import auth_router
 from app.api.v1.endpoints.project_endpoint import project_router
 from app.api.v1.endpoints.task_endpoint import task_router
+from app.api.v1.endpoints.user_endpoint import user_router
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(project_router)
 router.include_router(auth_router)
 router.include_router(task_router)
+router.include_router(user_router)
