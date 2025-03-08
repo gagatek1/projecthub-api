3c8b6c049d8271b85f4a17fae2fecea7d57c593c
gagatek1 <jakub.gagat@outlook.com>
2025-03-08 19:25:29 +0100

    Edit main

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from mangum import Mangum
 from app.api.v1.router import router
 from app.core.database import create_tables
 
-app = FastAPI()
+app = FastAPI(
+    title="ProjectHub API",
+    description="API to manage projects and tasks",
+    version="0.4.0",
+)
 
 handler = Mangum(app)
 
