7e9b233af34b30f947b53e403e9ab797cc20e923
gagatek1 <jakub.gagat@outlook.com>
2025-03-09 13:47:40 +0100

    Add get user service

--- app/services/user/get_service.py
+++ app/services/user/get_service.py
@@ -2,4 +2,8 @@ from app.core.cognito import Cognito
 
 
 def get_users(cognito: Cognito):
-    return cognito.get_users().get("Users")
+    return cognito.shows_users().get("Users")
+
+
+def get_user(id: str, cognito: Cognito):
+    return cognito.show_user(id)
