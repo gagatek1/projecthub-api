dee6a46ba8c081ecb3857ebce8c7c325e18ae8e7
gagatek1 <jakub.gagat@outlook.com>
2025-03-09 15:39:12 +0100

    Add email endpoint

--- app/api/v1/endpoints/user_endpoint.py
+++ app/api/v1/endpoints/user_endpoint.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends
 
 from app.core.cognito import Cognito
 from app.core.dependencies import get_cognito
+from app.models.user import UserEmail
 from app.services.user.get_service import get_user, get_users
+from app.services.user.update_service import get_token, update_email_service
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -19,3 +21,12 @@ async def show_user(user_id: str, cognito: Cognito = Depends(get_cognito)):
     user = get_user(user_id, cognito)
 
     return user
+
+
+@user_router.post("/email")
+async def update_email(
+    email: UserEmail,
+    user: dict = Depends(get_token),
+    cognito: Cognito = Depends(get_cognito),
+):
+    return update_email_service(email, user, cognito)
