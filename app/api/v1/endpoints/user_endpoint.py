5697708cfe95beeb8974481ce172a67eb535dd87
gagatek1 <jakub.gagat@outlook.com>
2025-03-09 13:48:27 +0100

    Add show user endpoint

--- app/api/v1/endpoints/user_endpoint.py
+++ app/api/v1/endpoints/user_endpoint.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from app.core.cognito import Cognito
 from app.core.dependencies import get_cognito
-from app.services.user.get_service import get_users
+from app.services.user.get_service import get_user, get_users
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -12,3 +12,10 @@ async def show_users(cognito: Cognito = Depends(get_cognito)):
     users = get_users(cognito)
 
     return users
+
+
+@user_router.get("/{user_id}")
+async def show_user(user_id: str, cognito: Cognito = Depends(get_cognito)):
+    user = get_user(user_id, cognito)
+
+    return user
